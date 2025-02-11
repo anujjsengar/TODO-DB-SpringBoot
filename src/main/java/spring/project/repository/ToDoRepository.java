976d12584f104956f7e07048b6b6db09fcc4670e
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2025-02-11 15:34:19 +0530

    Update ToDoRepository.java

--- src/main/java/spring/project/repository/ToDoRepository.java
+++ src/main/java/spring/project/repository/ToDoRepository.java
@@ -5,11 +5,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import spring.project.entity.ToDo;
 
-/**
- * @Author: apple
- * @created on 30/04/2020
- * @Project is SpringSimpleTodo
- */
 
 @Repository
 //public interface ToDoRepository extends PagingAndSortingRepository<ToDo, Long> {
